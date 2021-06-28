9b5dcd389db3261f9dc45d3599a77e4d62a4993c
Sydney Evans <sydneye143@gmail.com>
2021-06-28 11:59:59 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dojo.Models;
+
+namespace dojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost("Add")]
+        public IActionResult Add(Survey newSurvey)
+        {
+            Console.WriteLine($"Name:{newSurvey.Name}");
+            ViewBag.Name =newSurvey.Name;
+            ViewBag.Location =newSurvey.Location;
+            ViewBag.Language =newSurvey.Language;
+            ViewBag.Comment =newSurvey.Comment;
+
+            return View("Index");
+        }
+     
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace dojo.Models
+{
+    public class Survey
+    {
+        public string Name {get;set;}
+
+        public string Location {get;set;}
+
+        public string Language {get;set;}
+
+        public string Comment {get;set;}
+    }
+}
